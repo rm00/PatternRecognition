09e2302f2d76131b07639a1d9be88c6fada86f5e
Bob <roberto.meloni@mdotm.eu>
2021-06-10 11:48:47 +0200

    Initial commit

---
+++ src/main/java/com/rmeloni/PatternRecognition/PatternRecognitionApplication.java
@@ -0,0 +1,13 @@
+package com.rmeloni.PatternRecognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternRecognitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatternRecognitionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rmeloni/PatternRecognition/PatternRecognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmeloni.PatternRecognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatternRecognitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
